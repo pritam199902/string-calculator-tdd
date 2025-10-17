3e83bcfbf077a39e03e1f7ad10869d3919aa0d08
Pritam <www.pritamdas1999@gmail.com>
2025-10-17 13:06:54 +0530

    update: custom delimiter validation added

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -6,8 +6,21 @@ function add(numbers = "") {
   // delimiters to validate
   let delimiters = /,|\\n/;
 
-  // split the numbers by delimiters
-  let numberArray = numbers.split(delimiters).map(Number);
+  // validate for custom delimiter
+  if (nums.startsWith("//")) {
+    const firstNewLine = nums.indexOf("\\n");
+
+    if (firstNewLine === -1) {
+      throw new Error("Invalid custom delimiter format");
+    }
+
+    const customDelimiter = nums.substring(2, firstNewLine);
+    nums = nums.substring(firstNewLine + 2);
+    delimiters = new RegExp(`${customDelimiter}`);
+  }
+
+  // split the nums by delimiters
+  let numberArray = nums.split(delimiters).map(Number);
 
   // validate for negative numbers
   const negativeNumbers = numberArray.filter((num) => num < 0);
