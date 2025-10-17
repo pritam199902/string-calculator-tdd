5fac2c8646c8f28b7e167b827a64f13822f88882
Pritam <www.pritamdas1999@gmail.com>
2025-10-17 11:23:38 +0530

    update: exceptoin for negative number updated

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,11 +1,19 @@
 function add(numbers = "") {
   if (numbers == "") return 0;
 
-  // delimiters to validate as regxp
-  const delimiters = /,|\n/;
+  // delimiters to validate
+  let delimiters = /,|\n/;
 
   // split the numbers by delimiters
   let numberArray = numbers.split(delimiters).map(Number);
+
+  // validate for negative numbers
+  const negativeNumbers = numberArray.filter((num) => num < 0);
+  if (negativeNumbers.length > 0) {
+    throw new Error(
+      `negative numbers not allowed ${negativeNumbers.join(", ")}`
+    );
+  }
 
   // sum the numbers
   return numberArray.reduce((sum, num) => sum + num, 0);
